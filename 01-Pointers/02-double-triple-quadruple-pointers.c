1e1b95693925f8544f38eadf7571c62b10ae0b6a
hello-isa <isabeljoyadriatico@gmail.com>
2024-06-11 08:10:48 +0800

    Update code

--- 01-Pointers/02-double-triple-quadruple-pointers.c
+++ 01-Pointers/02-double-triple-quadruple-pointers.c
@@ -9,6 +9,22 @@ int main(){
 	char ****e = &d; // Quadruple pointer to a triple pointer
 	
 	printf("Address of a: %p \n", &a);
+	printf("Address of pointer b: %p \n", &b);
+	printf("Address of double pointer c: %p \n", &c);
+	printf("Address of triple pointer d: %p \n", &d);
+	printf("Address of quadruple pointer e: %p \n", &e);
+	
+	printf("---\n");
+	
+	printf("Value of a: %c \n", a);
+	printf("Value of a using pointer b: %c \n", *b);
+	printf("Value of a using double pointer c: %c \n", **c);
+	printf("Value of a using triple pointer d: %c \n", ***d);
+	printf("Value of a using quadruple pointer e: %c \n", ****e);
+	
+	printf("---\n");
+	
+	printf("Address of a: %p \n", &a);
 	printf("Address of a using pointer b: %p \n", b);
 	printf("Address of a using double pointer c: %p \n", *c);
 	printf("Address of a using triple pointer d: %p \n", **d);
