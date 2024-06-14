1962155a5644e768cbe4a8fd5b52b92d76635d44
hello-isa <isabeljoyadriatico@gmail.com>
2024-06-14 09:07:52 +0800

    Create 05-calculate-array-sum.c

---
+++ 01-Pointers/05-calculate-array-sum.c
@@ -0,0 +1,40 @@
+// Calculate Array Sum: 
+// Write a function that calculates the sum of all elements in an integer array using pointers.
+// Function name: calculateArraySum
+// Variables: int array[], int size
+#include<stdio.h>
+#define MAX 5
+
+void displayArray(int arr[], int size);
+int calculateArraySum(int arr[], int size);
+
+int main(){
+	int arrInt[MAX] = {11, 22, 33, 44, 55};
+	int n = MAX;
+	
+	displayArray(arrInt, n);
+	int sum = calculateArraySum(arrInt, n);
+	printf("Sum of the array: %d", sum);
+	 
+	return 0;
+}
+
+void displayArray(int arr[], int size){
+	int n;
+	for(n = 0; n < size; n++){
+		printf("%d ", arr[n]);
+	}
+	printf("\n");
+}
+
+int calculateArraySum(int arr[], int size){
+	int *start;
+	int *end;
+	int sum = 0;
+	
+	for(start = arr, end = arr + (size - 1); start <= end; start++){
+		sum = sum + *start;
+	}
+	
+	return sum;
+}
