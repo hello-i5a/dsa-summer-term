1be34836de935808c0136d6f3981036687cc39ba
hello-isa <isabeljoyadriatico@gmail.com>
2024-06-14 15:54:47 +0800

    Create 06-reverse-string.c

---
+++ 01-Pointers/06-reverse-string.c
@@ -0,0 +1,16 @@
+// Reverse String:
+// Write a function that takes a string pointer as an argument and reverses the string it points to.
+// Function name: reverseString
+// Variables: char *str
+#include <stdio.h>
+
+void reverseString(char *str);
+
+int main()
+{
+	return 0;
+}
+
+void reverseString(char *str)
+{
+}
