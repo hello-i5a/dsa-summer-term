3518a882c1e6a7b8c665f3ed5ebb8c0359826030
hello-isa <isabeljoyadriatico@gmail.com>
2024-06-18 07:03:44 +0800

    Update 06-reverse-string.c

--- 01-Pointers/06-reverse-string.c
+++ 01-Pointers/06-reverse-string.c
@@ -8,9 +8,35 @@ void reverseString(char *str);
 
 int main()
 {
+	char string[] = "Hi, Joe!"; // Sequence of characters with a null terminator
+
+	printf("%s \n", string);
+	reverseString(string);
+	printf("%s \n", string);
+
 	return 0;
 }
 
 void reverseString(char *str)
 {
+	// Calculate the length
+	int len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	// Initialize two pointers at the start and end
+	char *start = str;
+	char *end = str + (len - 1); // Exclude the null terminator
+								 // Calculates the pointer to the last character of the string
+
+	// Swap the pointers
+	// Strings are essentially array and are stored in contiguous memory hence the for-loop condition
+	for (; start < end; start++, end--)
+	{
+		char temp = *start;
+		*start = *end;
+		*end = temp;
+	}
 }
