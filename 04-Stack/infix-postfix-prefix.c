bd78dfe26d06adc17f97a6a9085b7baf8226c868
hello-isa <isabeljoyadriatico@gmail.com>
2024-07-06 17:01:45 +0800

    Update infix-postfix-prefix.c

--- 04-Stack/infix-postfix-prefix.c
+++ 04-Stack/infix-postfix-prefix.c
@@ -1,15 +1,15 @@
 /*
-1. Infix notation: a+b
-2. Postfix notation (Reverse Polish notation): ab+
-3. Prefix notation (Polish notation): +ab
+1. Infix notation:                                  a+b
+2. Postfix notation (Reverse Polish notation):      ab+
+3. Prefix notation (Polish notation):               +ab
 
 Operand: numbers/letters/variables
 Operators: symbols (PEMDAS)
-    Priority
-    ( )   -> 4
-    ^     -> 3
-    * /   -> 2
-    + -   -> 1
+                    Priority
+                    ( )   -> 4
+                    ^     -> 3
+                    * /   -> 2
+                    + -   -> 1
 
 Rules in Postfix:
 1. No operator of the same priority can stay after one another in the stack unless separated by a `(`.
