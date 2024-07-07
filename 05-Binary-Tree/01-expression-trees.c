bf0227a19dac8588539d8ccc762655c5966d4718
hello-isa <isabeljoyadriatico@gmail.com>
2024-07-07 18:27:24 +0800

    Create 01-expression-trees.c

---
+++ 05-Binary-Tree/01-expression-trees.c
@@ -0,0 +1,8 @@
+/*
+Expression Trees: a binary tree where the internal node represents an operator and each leaf node represents an operand.
+
+Traversal:
+Inorder: Similar to infix notation.
+Postoder: Similar to postorder notation (a.k.a. reverse Polish notation).
+Preorder: Similar to preorder notation (a.k.a. Polish notation).
+*/
