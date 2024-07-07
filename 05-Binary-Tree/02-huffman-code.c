94ce3e9a09c7afdf396efe3b695c75db83841db9
hello-isa <isabeljoyadriatico@gmail.com>
2024-07-08 00:16:39 +0800

    Create 02-huffman-code.c

---
+++ 05-Binary-Tree/02-huffman-code.c
@@ -0,0 +1,10 @@
+/*
+1. Count the frequency for each character.
+2. List out from the most frequent to the least frequent.
+3. Add the occurrences/frequency of the list frequent first.
+Keep on repeating until all characters are added.  Prioritize the leaf nodes.
+Tip: Always arrange in descending order.
+4. Create a code table:
+        Least frequent is 0.
+        Most frequent is 1.
+*/
