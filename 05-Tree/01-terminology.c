b67706c441e99398192b047adc865938e1a0a59a
hello-isa <isabeljoyadriatico@gmail.com>
2024-07-08 21:09:28 +0800

    Create 01-terminology.c

---
+++ 05-Tree/01-terminology.c
@@ -0,0 +1,8 @@
+/*
+Tree: non-linear data structure that is (1) hierarchical in nature and (2) uses recursive definition.
+    A collection of nodes (i.e. elements).
+        Node:    N       -> number of individual elements
+        Edge:    N-1     -> number of connecting links between two nodes
+
+
+*/
