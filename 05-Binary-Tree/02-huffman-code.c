5f4e7d0dfd5118b6083415102b0db004897a1663
Issa <58451917+hello-isa@users.noreply.github.com>
2024-07-08 13:09:16 +0800

    Update

--- 05-Binary-Tree/02-huffman-code.c
+++ 05-Binary-Tree/02-huffman-code.c
@@ -2,7 +2,7 @@
 1. Count the frequency for each character.
 2. List out from the most frequent to the least frequent.
 3. Add the occurrences/frequency of the list frequent first.
-Keep on repeating until all characters are added.  Prioritize the leaf nodes.
+Keep on repeating until all characters are added.  Prioritize the leaf nodes (i.e. the characters).
 Tip: Always arrange in descending order.
 4. Create a code table:
         Least frequent is 0.
