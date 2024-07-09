a4d7ecac0294fbc1c869ee8ec3efc22d41755638
Issa <58451917+hello-isa@users.noreply.github.com>
2024-07-09 10:53:39 +0800

    Create 03-binary-search-tree.c

---
+++ 06-Binary-Tree/03-binary-search-tree.c
@@ -0,0 +1,20 @@
+/*
+Insertion in BST
+1. Maintain the BST property.
+
+Deletion in BST
+Case 1: Delete a leaf node
+Case 2: Delete a node with single child
+Case 3: Delete a node with both children
+	Resulting tree follows the BST property.
+	Find the inorder successor of the node.
+Case 4: Delete the root
+
+Search in BST
+1. Think of BST as a sorted array.
+2. Perform Binary Search algorithm.
+3. Return True if found, else False.
+
+BST Simulator:
+https://www.cs.usfca.edu/~galles/visualization/BST.html
+*/
