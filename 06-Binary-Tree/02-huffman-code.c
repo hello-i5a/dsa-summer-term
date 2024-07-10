109d2cc40897a294731781bb441692f4b2ea82c9
hello-isa <isabeljoyadriatico@gmail.com>
2024-07-11 01:44:46 +0800

    Update

--- 06-Binary-Tree/02-huffman-code.c
+++ 06-Binary-Tree/02-huffman-code.c
@@ -1,5 +1,5 @@
 /*
-Huffman code: a binary tree. It is a method of lossless data compression that uses variable-length codes to represent characters or symbols. 
+Huffman code: a binary tree. It is a method of lossless data compression that uses variable-length codes to represent characters or symbols.
                 It assigns shorter codes to more frequently occurring characters, optimizing the efficiency of the encoding process.
                 Each node can have up to two children, and the tree is built specifically for encoding and decoding purposes based on the frequency of characters or symbols.
 
@@ -12,6 +12,16 @@ Tip: Always arrange in descending order.
         Least frequent is 0.
         Most frequent is 1.
         If similar frequency, left node is 0 and right node is 1.
+
+To get the average code length:
+        P * No. of B -> Summation
+                Probability of each character, P
+                Bits of each character, B
+
+To get the number of bytes to allocate for the text using Huffman code:
+        F * No. of B -> Summation (in bits) / 8 bits -> Round up the answer (in Bytes)
+                Frequency of each character, F
+                Bits of each character, B
 
 Resources:
 Huffman Tree Generator:
