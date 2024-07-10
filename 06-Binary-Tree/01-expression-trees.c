cbfa3b80298867c9e95a5bb76d9d41314d3fe8dd
hello-isa <isabeljoyadriatico@gmail.com>
2024-07-10 23:33:07 +0800

    Update

--- 06-Binary-Tree/01-expression-trees.c
+++ 06-Binary-Tree/01-expression-trees.c
@@ -1,7 +1,7 @@
 /*
 Expression Trees: a binary tree where the internal node represents an operator and each leaf node represents an operand.
 
-Traversal:
+Traversal (specifically Depth First Search (DFS)):
 Inorder: Similar to infix notation.
 Postorder: Similar to postorder notation (a.k.a. reverse Polish notation).
 Preorder: Similar to preorder notation (a.k.a. Polish notation).
