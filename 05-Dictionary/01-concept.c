e2279545c0a4f932907ebf1dcdf8c940d4facdc3
hello-isa <isabeljoyadriatico@gmail.com>
2024-07-12 21:43:20 +0800

    Update

---
+++ 05-Dictionary/01-concept.c
@@ -0,0 +1,13 @@
+/*
+Two types of Dictionary:
+1. Open Dictionary / Open Addressing
+    Separate Chaining
+2. Closed Dictionary / Closed Address
+    Linear Probing
+    Quadratic Probing
+    Double Hashing
+
+Terms:
+Collision
+Packing Density
+*/
