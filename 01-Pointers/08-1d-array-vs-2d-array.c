721c1af02269db6ab8b7224c1a7fda059a6b0963
hello-isa <isabeljoyadriatico@gmail.com>
2024-07-14 10:25:02 +0800

    Create 08-1d-array-vs-2d-array.c

---
+++ 01-Pointers/08-1d-array-vs-2d-array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[5] = {10, 20, 30, 40, 50};
+
+    int n = 0;
+    for (; n < 5; n++)
+    {
+        printf("Address of index %d: %p\n", n, &arr[n]);
+    }
+    printf("Address of the first index: %p\n", arr); // Pointer
+
+    printf("\n==========\n\n");
+
+    int multi[2][4];
+    printf("Address of multi: %u\n", multi);       // Pointer
+    printf("Address of multi[0]: %u\n", multi[0]); // Pointer
+    printf("Address of multi[0][0]: %u\n", &multi[0][0]);
+
+    return 0;
+}
